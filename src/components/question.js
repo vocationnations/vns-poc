c25ae08a6de7f155f02cc0b63ccb83bc3ea05728
tayabsoomro <isoomro.t@gmail.com>
2021-01-17 21:44:21 -0600

    attach the scores back to the question UI so that the percentages are calculated correctly

--- src/components/question.js
+++ src/components/question.js
@@ -1,64 +1,67 @@
-import React, { useState} from 'react';
-import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import React, {useEffect, useState} from 'react';
+import {DragDropContext, Draggable, Droppable} from "react-beautiful-dnd";
 
 
 const Sentence = ({sentence, index}) => {
   return (
-    <Draggable draggableId={String(index+1)} index={index}>
-      {(provided, snapshot) => (
-          <div
-              ref={provided.innerRef}
-              {...provided.draggableProps}
-              {...provided.dragHandleProps}
-          >
-            <span className="btn btn-info mb-2 w-100">{sentence.content}</span>
-          </div>
-      )}
+      <Draggable draggableId={String(index + 1)} index={index}>
+        {provided => (
+            <div
+                ref={provided.innerRef}
+                {...provided.draggableProps}
+                {...provided.dragHandleProps}
+            >
+              <span className="btn btn-info mb-2 w-100">{sentence.content}</span>
+            </div>
+        )}
     </Draggable>
   )
 }
 
 
 const Question = ({question_data, setScores, advanceQuestion, questionNumber}) => {
-  const [buttonType, setButtonType] = useState('');
-  const [isSelected, setIsSelected] = useState(false);
 
+  const sntnces = [
+    {id: 'clan', content: question_data.clan_option},
+    {id: 'adhocracy', content: question_data.adhocracy_option},
+    {id: 'market', content: question_data.market_option},
+    {id: 'hierarchy', content: question_data.hierarchy_option},
+  ]
 
   const [state, setState] = useState({
-    sentences: [
-      { id: 'clan', content: question_data.clan_option },
-      { id: 'adhocracy', content: question_data.adhocracy_option },
-      { id: 'market', content: question_data.market_option },
-      { id: 'hierarchy', content: question_data.hierarchy_option },
-    ]
+    sentences: []
   });
+
+  useEffect(() => {
+    console.log(sntnces);
+    setState({
+      sentences: sntnces
+    })
+  }, [question_data])
+
 
   // Reorder the list items
   const reorder = (list, startIndex, endIndex) => {
-      const result = Array.from(list);
-      const [removed] = result.splice(startIndex, 1);
-      result.splice(endIndex, 0, removed);
+    const result = Array.from(list);
+    const [removed] = result.splice(startIndex, 1);
+    result.splice(endIndex, 0, removed);
 
-      return result;
+    return result;
   };
 
-  const handleOptionSelect = (opt_type) => {
-    setButtonType(opt_type);
-    setIsSelected(true);
-  }
+  const saveScore = () => {
+    let culture = {}
+    for (let i = 0; i < state.sentences.length; i++) {
+      culture[String(state.sentences[i].id)] = state.sentences.length - i;
+    }
 
-  const saveScore = (type) => {
     setScores(
-      prev => ({
-        ...prev,
-        [String(questionNumber)]: {
-          ...prev.[String(questionNumber)],
-          [type]: 1
-        }
-      })
+        prev => ({
+          ...prev,
+          [String(questionNumber)]: culture
+        })
     )
     advanceQuestion();
-    setIsSelected(false);
   }
 
   const onDragEnd = (result) => {
@@ -78,7 +81,7 @@ const Question = ({question_data, setScores, advanceQuestion, questionNumber}) =
 
     setState({ sentences });
   }
-
+  console.log("DOING THIS AGAIN");
   return (
     <div className="card">
       <div className="card-body">
@@ -88,20 +91,20 @@ const Question = ({question_data, setScores, advanceQuestion, questionNumber}) =
         <div className="d-flex flex-column col-lg-5 mx-auto text-center">
           <DragDropContext onDragEnd={onDragEnd}>
             <Droppable droppableId="list">
-              {provided => (
-                <div ref={provided.innerRef} {...provided.droppableProps}>
-                  {
-                    state.sentences.map((s,k) => {
-                      return <Sentence sentence={s} index={k} key={k} />
-                    })
-                  }
-                  {provided.placeholder}
-                </div>
+              {(provided, snapshot) => (
+                  <div ref={provided.innerRef} {...provided.droppableProps}>
+                    {
+                      state.sentences.map((s, k) => {
+                        return <Sentence sentence={s} index={k} key={k}/>
+                      })
+                    }
+                    {provided.placeholder}
+                  </div>
               )}
             </Droppable>
           </DragDropContext>
         </div>
-        <button className="btn btn-primary" disabled={!isSelected} onClick={() => saveScore(buttonType)}>Next</button>
+        <button className="btn btn-primary" onClick={() => saveScore()}>Next</button>
       </div>
     </div>
   );
