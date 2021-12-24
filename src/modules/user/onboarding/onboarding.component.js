f4ebdb35fc1a30df242a1a39568e3ca479b07ce5
tayabsoomro <isoomro.t@gmail.com>
2021-12-24 00:49:00 -0600

    create the job-selection onboarding step

--- src/modules/user/onboarding/onboarding.component.js
+++ src/modules/user/onboarding/onboarding.component.js
@@ -1,13 +1,78 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import AuthService from "../../auth/auth.service";
 import {useHistory} from "react-router-dom";
+import {
+    OnBoardingClimateEntryComponent,
+    OnBoardingCultureEntryComponent,
+    OnBoardingJobSelectionComponent,
+    OnBoardingSkillsSelectionComponent
+} from "./steps/index"
 
 const auth_service = new AuthService();
+
+const OnBoardingMessage = ({setStepNumber}) => {
+    return (
+        <div className="container text-center">
+            <h3>Welcome to VocationNations!</h3>
+            <div className="vspacer-20"/>
+            <img alt={"Hello"}
+                 style={{width: "15%"}}
+                 src="/images/onboarding/hello.png"/>
+            <p className="lead w-50 mx-auto border-black pl-5 pr-5">
+                Thank you very much for signing up to VocatioNations.
+                We are very excited to have you on board. We would like to know
+                a little bit more about you.
+                Lets go through some steps so that we can refine your search and
+                increase your chances for perfect matches!
+            </p>
+            <button className="btn btn-success"
+                    onClick={() => setStepNumber(1)}>Get Started!
+            </button>
+        </div>
+    );
+}
+
 
 const OnBoardingComponent = () => {
 
     const history = useHistory()
+
+    const [stepNumber, setStepNumber] = useState(0);
+
+    const advanceStep = () => {
+
+        console.log("STEP NUMBER: " + stepNumber);
+        if (stepNumber < steps.length) {
+            setStepNumber((prev) => {
+                return prev + 1;
+            })
+        }
+    }
+
+    const steps = [
+        {
+            name     : 'Job Selection',
+            component: <OnBoardingJobSelectionComponent
+                advanceStep={advanceStep}/>
+        },
+        {
+            name     : 'Skills Selection',
+            component: <OnBoardingSkillsSelectionComponent
+                advanceStep={advanceStep}/>
+        },
+        {
+            name     : 'Culture Entry',
+            component: <OnBoardingCultureEntryComponent
+                advanceStep={advanceStep}/>
+        },
+        {
+            name     : 'Climate Entry',
+            component: <OnBoardingClimateEntryComponent
+                advanceStep={advanceStep}/>
+        }
+    ]
+
 
     const logout = () => {
         auth_service.userLogout();
@@ -17,8 +82,33 @@ const OnBoardingComponent = () => {
 
     return (
         <div>
-            <h1>OnBoarding!</h1>
-            <button onClick={() => logout()}>Logout</button>
+            <div className="container-fluid">
+                <div className="vspacer-10"/>
+                <div className="module-stepbar d-flex">
+                    <ul className="steps six clearfix justify-content-center"
+                        id="step-buttons">
+                        {
+                            steps.map((s, i) => {
+                                return <li key={i}
+                                           className={stepNumber === i + 1 ? 'active' : ''}><span
+                                    className="step-no">{i + 1}</span>{s.name}
+                                </li>
+                            })
+                        }
+                    </ul>
+                </div>
+                <hr/>
+                {
+                    steps.map((r, i) => {
+                        return stepNumber === i + 1 &&
+                            <div key={i}>{r.component}</div>;
+                    })
+                }
+                {
+                    stepNumber === 0 &&
+                    <OnBoardingMessage setStepNumber={setStepNumber}/>
+                }
+            </div>
         </div>
     );
 }
---
+++ src/modules/user/onboarding/steps/onboarding-job-selection/onboarding-job-selection.component.js
@@ -0,0 +1,42 @@
+import React, {useEffect, useState} from 'react';
+
+import './onboarding-job-selection.css';
+import AutoComplete from "../../../../../components/autocomplete/autocomplete";
+import OnBoardingService from "../../onboarding.service";
+
+const service = new OnBoardingService();
+
+const OnBoardingJobSelectionComponent = () => {
+
+    const [keyword, setKeyword]                 = useState('')
+    const [listSuggestions, setListSuggestions] = useState([]);
+    const [loading, setLoading]                 = useState(false);
+
+    useEffect(() => {
+        if (keyword !== '') {
+            setLoading(true);
+            service.getOccupationsByKeyword(
+                keyword,
+                (data) => {
+                    setLoading(false);
+                    let suggestions = data.occupation.map(({title}) => title)
+                    setListSuggestions(suggestions)
+                },
+                (error) => {
+                    setLoading(false);
+                    console.log(error)
+                }
+            )
+
+        }
+    }, [keyword])
+
+    return (<div className="container-fluid text-center">
+        <h4>What type of work are you looking for?</h4>
+        <div className="vspacer-20"/>
+        <AutoComplete setKeyword={setKeyword} listSuggestions={listSuggestions}
+                      loading={loading}/>
+    </div>);
+};
+
+export default OnBoardingJobSelectionComponent;
