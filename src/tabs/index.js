a2cdbab193d78adbac71dc036dc8e39e87ac46b7
Tayab Soomro <tayab.soomro@dal.ca>
2022-03-12 19:44:52 -0400

    remove unused tabs

--- src/tabs/index.js
+++ src/tabs/index.js
@@ -1,9 +1,5 @@
 import CultureSelectionTab from './culture';
-import SkillsSelectionTab from './skills';
-import WorthTab from "./worth";
 
 export {
   CultureSelectionTab,
-  SkillsSelectionTab,
-  WorthTab,
 }
